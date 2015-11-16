79aa3de49e8b096a1d831284729be28a0b8f687e
AlbertoBonfiglio <Alberto.Bonfiglio@gmail.com>
2015-11-16 12:55:46 -0800

    Initial Commit

--- exercise2/keylogger_node_start.py
+++ exercise2/keylogger_node_start.py
@@ -5,5 +5,6 @@ from nodes.key_logger import KeyLogger
 if __name__ == '__main__':
 
     _keyLoggerControl = KeyLogger()
+
     while not _keyLoggerControl.isShutDown():
         _keyLoggerControl.run()
